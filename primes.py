bea09221b8b6aafe90364715bc8f61d3bf02c4d9
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2018-12-02 02:22:51 +0300

    reworked primitive root function to improve work speed

--- primes.py
+++ primes.py
@@ -86,27 +86,34 @@ def gcd(a, b):
     :param b: value to find divisor
     :return: greatest common divisor between a and b
     """
-    while b != 0:
-        a, b = b, a % b
-    return a
+    if not b:
+        return a
+    return gcd(b, a % b)
 
 
-def primitive_roots(module):
+def primitive_roots(p):
     """
     Finding list of primitive roots of num_p
-    :param module: value to find primitive roots
+    :param p: value to find primitive roots
     :return: list of primitive roots
     """
-    roots = []
-    required_set = set(num for num in range(1, module) if gcd(num, module) == 1)
+    if p == 2:
+        return 1
+        # the prime divisors of p-1 are 2 and (p-1)/2 because
+        # p = 2x + 1 where x is a prime
+    p1 = 2
+    p2 = (p - 1) // p1
 
-    for g in range(1, module):
-        actual_set = set(pow(g, powers) % module for powers in range(1, module))
-        if required_set == actual_set:
-            roots.append(g)
-    return roots
+    # test random g's until one is found that is a primitive root mod p
+    while True:
+        g = random.randint(2, p - 1)
+        # g is a primitive root if for all prime factors of p-1, p[i]
+        # g^((p-1)/p[i]) (mod p) is not equal to 1
+        if not (pow(g, (p - 1) // p1, p) == 1):
+            if not pow(g, (p - 1) // p2, p) == 1:
+                return g
 
 
 if __name__ == '__main__':
     print(generate_large_prime())
-    print(primitive_roots(13))
+    print(primitive_roots(generate_large_prime()))
